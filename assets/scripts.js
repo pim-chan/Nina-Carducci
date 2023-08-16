6d62010fc32f73d3af1a2bcba26c9478870f8864
pim-chan <tamba.chloe@outlook.fr>
2023-08-17 00:10:27 +0200

    Finalisation carrousel

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,74 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+// CAROUSEL
+let currentSlide = 0;
+let nbSlides;
+
+const indicatorCreation = () => {
+    fetch('./assets/images.json')
+    .then(response => response.json())
+    .then(imagesData => {
+        nbSlides = imagesData.slider.length -1;
+
+        imagesData.slider.forEach((image, index) => { 
+        const navigatorContainer = document.querySelector(".indicators-container")
+        const navigatorElement = document.createElement('button');
+        navigatorElement.classList.add('indicator')
+        if(index === 0){navigatorElement.classList.add('indicator-selected');}
+        navigatorContainer.appendChild(navigatorElement);
     });
-});
+  })
+  .catch(error => {
+    console.error("Une erreur s'est produite lors du chargement du fichier JSON:", error);
+  });
+}
+
+const indicatorSelection = (indicatorIndex) => {
+  Array.from(document.querySelectorAll('.indicator')).forEach((element,index) => {
+      element.classList.remove('indicator-selected');
+      if(index === indicatorIndex){element.classList.add('indicator-selected');
+    }
+  });
+}
+
+const slide = (direction) => {
+  fetch('./assets/images.json')
+    .then(response => response.json())
+    .then(imagesData => {
+        const slidesImages = imagesData.slider;
+        console.log(slidesImages);
+        currentSlide = currentSlide + direction;
+
+        if(currentSlide === -1){
+            currentSlide = nbSlides;
+        } else if(currentSlide > nbSlides){
+            currentSlide = 0;
+        }
+        
+        document.querySelector('.slide__img').setAttribute('src', slidesImages[currentSlide].image);
+
+        indicatorSelection(currentSlide);
+  })
+  .catch(error => {
+    console.error("Une erreur s'est produite lors du chargement du fichier JSON:", error);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const svgButtons = document.querySelectorAll("svg[data-action]");
+
+  svgButtons.forEach(button => {
+      button.addEventListener("click", () => {
+          const action = button.getAttribute("data-action");
+          if (action === "prev") {
+              slide(-1);
+          } else if (action === "next") {
+              slide(1);
+          }
+      });
+  });
+})
+
+indicatorCreation()
+
+// MODAL
+
+
